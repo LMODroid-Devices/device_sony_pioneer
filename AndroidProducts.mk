f4cded2f3c02760883515d93414fe5ee969445d4
LMO GerritBot <gerrit@libremobileos.com>
2023-11-03 04:20:43 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_pioneer.mk
+    $(LOCAL_DIR)/lmodroid_pioneer.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_pioneer-user \
-    lineage_pioneer-userdebug \
-    lineage_pioneer-eng
+    lmodroid_pioneer-user \
+    lmodroid_pioneer-userdebug \
+    lmodroid_pioneer-eng
